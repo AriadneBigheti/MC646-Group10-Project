cb65ba8d6731c6cc42b45072d6b185c93e72e723
Ariadne Bigheti <ariadne.bigheti@hotmail.com>
2024-10-08 19:38:07 -0300

    FraudDetectionSystemTest: add new test case for normal transactions, refactor other tests

--- src/test/java/activity/FraudDetectionSystemTest.java
+++ src/test/java/activity/FraudDetectionSystemTest.java
@@ -4,9 +4,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import activity.FraudDetectionSystem.FraudCheckResult;
 import activity.FraudDetectionSystem.Transaction;
@@ -21,7 +21,7 @@ public class FraudDetectionSystemTest {
     }
 
     @Test
-    public void TC1() {
+    void testHighAmountTransaction() {
         // given
         Transaction transaction = new Transaction(11000, LocalDateTime.now(), "Campinas");
         List<Transaction> transactionsList = new ArrayList<>();
@@ -31,20 +31,20 @@ public class FraudDetectionSystemTest {
         FraudCheckResult result = sut.checkForFraud(transaction, transactionsList, blackList);
 
         // then
+        assertTrue(result.isFraudulent);
         assertTrue(result.verificationRequired);
-        assertTrue(result.riskScore >= 50);
-        assertTrue(result.riskScore <= 100);
+        assertEquals(50, result.riskScore);
     }
 
     @Test
-    public void TC2() {
+    void testExcessiveTransactionsWithinLastHour() {
         // given
         Transaction transaction = new Transaction(100, LocalDateTime.now(), "Campinas");
         List<String> blackList = new ArrayList<>();
         List<Transaction> transactionsList = new ArrayList<>();
     
         for (int i = 0; i < 11; i++) {
-            transactionsList.add(new Transaction(50, LocalDateTime.now(), "Campinas"));
+            transactionsList.add(new Transaction(50, LocalDateTime.now().minusMinutes(i+10), "Campinas"));
         }
 
         // when
@@ -52,29 +52,29 @@ public class FraudDetectionSystemTest {
 
         // then
         assertTrue(result.isBlocked);
-        assertTrue(result.riskScore >= 30);
-        assertTrue(result.riskScore <= 100);
+        assertEquals(30, result.riskScore);
+
     }
 
     @Test
-    public void TC3() {
+    void testLocationChangeWithinShortTime() {
         // given
         Transaction transaction = new Transaction(100, LocalDateTime.now(), "Campinas");
         List<Transaction> transactionsList = new ArrayList<>();
-        transactionsList.add(new Transaction(100, LocalDateTime.now(), "Sao Paulo"));
+        transactionsList.add(new Transaction(100, LocalDateTime.now().minusMinutes(10), "Sao Paulo"));
         List<String> blackList = new ArrayList<>();
 
         // when
         FraudCheckResult result = sut.checkForFraud(transaction, transactionsList, blackList);
 
         // then
+        assertTrue(result.isFraudulent);
         assertTrue(result.verificationRequired);
-        assertTrue(result.riskScore >= 20);
-        assertTrue(result.riskScore <= 100);
+        assertEquals(20, result.riskScore);
     }
 
     @Test
-    public void TC4() {
+    void testTransactionInBlacklistedLocation() {
         // given
         Transaction transaction = new Transaction(100, LocalDateTime.now(), "Campinas");
         List<Transaction> transactionsList = new ArrayList<>();
@@ -86,8 +86,25 @@ public class FraudDetectionSystemTest {
 
         // then
         assertTrue(result.isBlocked);
-        assertTrue(result.riskScore == 100); 
+        assertEquals(100, result.riskScore);
     }
 
+    @Test
+    void testNormalTransaction() {
+        LocalDateTime now = LocalDateTime.now();
+        Transaction transaction = new Transaction(500, now, "Campinas");
+        List<Transaction> transactionsList = new ArrayList<>();
+        transactionsList.add(new Transaction(1000, now.minusHours(3), "Campinas"));
+        List<String> blackList = new ArrayList<>();
+        blackList.add("Sao Paulo");
 
+        // when
+        FraudDetectionSystem.FraudCheckResult result = sut.checkForFraud(transaction, transactionsList, blackList);
+
+        // then
+        assertFalse(result.isFraudulent);
+        assertFalse(result.isBlocked);
+        assertFalse(result.verificationRequired);
+        assertEquals(0, result.riskScore);
+    }
 }
